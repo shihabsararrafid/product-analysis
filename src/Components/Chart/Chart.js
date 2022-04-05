50092594336eaf2fd2b7572a96f3b85c2a2e707d
shiabsararrafid <shrafid.532@gmail.com>
2022-04-05 12:18:06 +0600

    dashboard chart added

---
+++ src/Components/Chart/Chart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Bar, BarChart, CartesianGrid, Legend, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+
+const Chart = (props) => {
+
+    const newItem = props.data;
+    console.log(newItem)
+
+    return (
+        <div>
+
+
+        </div>
+    );
+};
+
+export default Chart;
--- src/Components/DashBoard/DashBoard.js
+++ src/Components/DashBoard/DashBoard.js
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import Chart from '../Chart/Chart';
+
 
 const DashBOard = () => {
+    const data1 = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
+
+    // const [data1, setData] = useState([]);
+    // useEffect(() => {
+    //     fetch('data.json')
+
+    //         .then(data1 => setData(data1))
+    // }, [data1])
     return (
         <div>
-            <h1>DashBoard</h1>
+            <div>
+                <LineChart width={730} height={250} data={data1}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="investment" stroke="#8884d8" />
+                    <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+                </LineChart>
+            </div>
+            <div>
+                <BarChart width={730} height={250} data={data1}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="investment" fill="#8884d8" />
+                    <Bar dataKey="revenue" fill="#82ca9d" />
+                </BarChart>
+            </div>
+
         </div>
+
+
+
     );
 };
 
